fdf6038504a7e809dea50c826e5d78b192bdb51f
Pakizamutallim <62bq5tn@code.edu.az>
2024-07-21 20:32:02 +0400

    added

--- ES6_classes/9-main.js
+++ ES6_classes/9-main.js
@@ -1,9 +1,5 @@
-import { listOfStudents } from "./9-hoisting.js";
+import { listOfStudents } from './9-hoisting.js';
 
-console.log(listOfStudents);
-
-const listPrinted = listOfStudents.map(
-    student => student.fullStudentDescription
-);
-
-console.log(listPrinted);
+test("listOfStudents has the correct length", () => {
+  expect(listOfStudents.length).toBe(5);
+});
