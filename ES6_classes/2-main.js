753533ae402806031a7ac0dc36ea82b9afe2bfc5
Pakizamutallim <62bq5tn@code.edu.az>
2024-07-21 19:51:16 +0400

    added

---
+++ ES6_classes/2-main.js
@@ -0,0 +1,20 @@
+// 2-main.js
+
+import HolbertonCourse from "./2-hbtn_course.js";
+
+const c1 = new HolbertonCourse("ES6", 1, ["Bob", "Jane"]);
+console.log(c1.name); // Output: ES6
+c1.name = "Python 101";
+console.log(c1);
+
+try {
+  c1.name = 12;
+} catch(err) {
+  console.log(err); // Output: TypeError: Name must be a string
+}
+
+try {
+  const c2 = new HolbertonCourse("ES6", "1", ["Bob", "Jane"]);
+} catch(err) {
+  console.log(err); // Output: TypeError: Length must be a number
+}
