b037b0284b4881cbd4ccc55b8cfe3744f9543b7f
Pakizamutallim <62bq5tn@code.edu.az>
2024-07-21 20:25:59 +0400

    added

---
+++ ES6_classes/10-car.js
@@ -0,0 +1,47 @@
+const brandSymbol = Symbol('brand');
+const motorSymbol = Symbol('motor');
+const colorSymbol = Symbol('color');
+
+export default class Car {
+  constructor(brand, motor, color) {
+    this[brandSymbol] = brand;
+    this[motorSymbol] = motor;
+    this[colorSymbol] = color;
+  }
+
+  get brand() {
+    return this[brandSymbol];
+  }
+
+  set brand(value) {
+    this[brandSymbol] = value;
+  }
+
+  get motor() {
+    return this[motorSymbol];
+  }
+
+  set motor(value) {
+    this[motorSymbol] = value;
+  }
+
+  get color() {
+    return this[colorSymbol];
+  }
+
+  set color(value) {
+    this[colorSymbol] = value;
+  }
+
+  cloneCar() {
+    // Create a new instance of Car
+    const clonedCar = new this.constructor();
+    
+    // Copy attributes from the current instance to the new instance
+    clonedCar[brandSymbol] = this[brandSymbol];
+    clonedCar[motorSymbol] = this[motorSymbol];
+    clonedCar[colorSymbol] = this[colorSymbol];
+    
+    return clonedCar;
+  }
+}
