5c2ada18e870a5bf42b88a5da90c30e6a98d0db8
Pakizamutallim <62bq5tn@code.edu.az>
2024-07-21 19:01:24 +0400

    added

---
+++ ES6_classes/eslint.config.js
@@ -0,0 +1,36 @@
+// eslint.config.js
+
+export default [
+  {
+    files: ['*.js'],
+    languageOptions: {
+      ecmaVersion: 2018,
+      sourceType: 'module',
+    },
+    globals: {
+      Atomics: 'readonly',
+      SharedArrayBuffer: 'readonly',
+    },
+    rules: {
+      'max-classes-per-file': 'off',
+      'no-underscore-dangle': 'off',
+      'no-console': 'off',
+      'no-shadow': 'off',
+      'no-restricted-syntax': [
+        'error',
+        'LabeledStatement',
+        'WithStatement',
+      ],
+    },
+    plugins: ['jest'],
+    env: {
+      browser: false,
+      es6: true,
+      jest: true,
+    },
+    extends: [
+      'airbnb-base',
+      'plugin:jest/all',
+    ],
+  },
+];
