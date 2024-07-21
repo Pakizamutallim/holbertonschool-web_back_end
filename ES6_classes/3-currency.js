f6bb84dc0fe0b39e02bf7cc0b76a84c9b3e6e35a
Pakizamutallim <62bq5tn@code.edu.az>
2024-07-21 19:54:06 +0400

    added

---
+++ ES6_classes/3-currency.js
@@ -0,0 +1,47 @@
+// 3-currency.js
+
+export default class Currency {
+    constructor(code, name) {
+      this._code = this._validateCode(code);
+      this._name = this._validateName(name);
+    }
+  
+    // Getter and Setter for code
+    get code() {
+      return this._code;
+    }
+  
+    set code(newCode) {
+      this._code = this._validateCode(newCode);
+    }
+  
+    // Getter and Setter for name
+    get name() {
+      return this._name;
+    }
+  
+    set name(newName) {
+      this._name = this._validateName(newName);
+    }
+  
+    // Method to display the full currency
+    displayFullCurrency() {
+      return `${this._name} (${this._code})`;
+    }
+  
+    // Private validation methods
+    _validateCode(code) {
+      if (typeof code !== 'string') {
+        throw new TypeError('Code must be a string');
+      }
+      return code;
+    }
+  
+    _validateName(name) {
+      if (typeof name !== 'string') {
+        throw new TypeError('Name must be a string');
+      }
+      return name;
+    }
+  }
+  
