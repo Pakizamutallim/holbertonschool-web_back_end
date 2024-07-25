768a8ac8b401576cee6713eb87aa85da46fe59b3
Pakizamutallim <62bq5tn@code.edu.az>
2024-07-25 18:57:16 +0400

    aded

--- ES6_data_manipulation/7-has_array_values.js
+++ ES6_data_manipulation/7-has_array_values.js
@@ -1,5 +1,5 @@
 // 7-has_array_values.js
 
-const hasValuesFromArray = (set, array) => array.every(value => set.has(value));
+const hasValuesFromArray = (set, array) => array.every((value) => set.has(value));
 
 export default hasValuesFromArray;
