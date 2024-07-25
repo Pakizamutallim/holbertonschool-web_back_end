87334b20548ab56f1ab0345c4579007685065890
Pakizamutallim <62bq5tn@code.edu.az>
2024-07-25 16:32:54 +0400

    aded

--- ES6_data_manipulation/2-get_students_by_loc.js
+++ ES6_data_manipulation/2-get_students_by_loc.js
@@ -1,6 +1,5 @@
 // 2-get_students_by_loc.js
 
-const getStudentsByLocation = (students, city) =>
-  students.filter(student => student.location === city);
+const getStudentsByLocation = (students, city) =>(students.filter(student => student.location === city));
 
 export default getStudentsByLocation;
