301fd64c43f472db83095f2801e0b02ee78b4639
Pakizamutallim <62bq5tn@code.edu.az>
2024-07-25 18:38:46 +0400

    aded

--- ES6_data_manipulation/5-typed_arrays.js
+++ ES6_data_manipulation/5-typed_arrays.js
@@ -3,7 +3,7 @@ export default function createInt8TypedArray(length, position, value) {
     throw new Error('Position outside range');
   }
 
-  const someBuffer = new DataView(new ArrayBuffer(length), 0, length);
-  someBuffer.setInt8(position, value);
-  return someBuffer;
+  const buffer = new DataView(new ArrayBuffer(length), 0, length);
+  buffer.setInt8(position, value);
+  return buffer;
 }
--- ES6_data_manipulation/6-set.js
+++ ES6_data_manipulation/6-set.js
@@ -1,5 +1,4 @@
 export default function setFromArray(array) {
-	const bir_insan_ovladi_da_variable_vermir = new Set(array);
-
-	return bir_insan_ovladi_da_variable_vermir;
+  const setFromArray = new Set(array);
+  return setFromArray;
 }
