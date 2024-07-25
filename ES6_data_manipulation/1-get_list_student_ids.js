53abd18e2a511db2804aef960a6ce676c646bc39
Pakizamutallim <62bq5tn@code.edu.az>
2024-07-25 16:25:00 +0400

    aded

--- ES6_data_manipulation/1-get_list_student_ids.js
+++ ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,11 +1,10 @@
 // 1-get_list_student_ids.js
 
-function getListStudentIds(arr) {
-  if (!Array.isArray(arr)) {
+const getListStudentIds = (students) => {
+  if (!Array.isArray(students)) {
     return [];
   }
-
-  return arr.map(student => student.id);
+  return students.map((student) => student.id);
 };
 
 export default getListStudentIds;
