a836fe3caeca968304aa13c3e9b14ac39765e87b
Pakizamutallim <62bq5tn@code.edu.az>
2024-08-13 15:53:35 +0400

    created file

---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""
+This module provides a type-annotated function for concatenating two strings.
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+    Concatenates two strings and returns the result.
+
+    Parameters:
+    str1 (str): The first string to concatenate.
+    str2 (str): The second string to concatenate.
+
+    Returns:
+    str: The concatenated string.
+    """
+    return str1 + str2
