b2e6f160df850fe246055c3b49a11bac4f43322a
Pakizamutallim <62bq5tn@code.edu.az>
2024-08-14 21:01:50 +0400

    created file

--- python_async_function/3-tasks.py
+++ python_async_function/3-tasks.py
@@ -1,24 +1,43 @@
 #!/usr/bin/env python3
 """
-This is a function
+This module provides a function to create asyncio Tasks from a coroutine
+that waits for a random delay.
+
+Functions:
+    - task_wait_random(max_delay: int) -> asyncio.Task:
+      Creates an asyncio Task that runs the wait_random coroutine.
+
+The `wait_random` coroutine is imported dynamically using the `__import__`
+function from a module named '0-basic_async_syntax'.
 """
 
-
 import asyncio
-import importlib
 
-module_name = "0_basic_async_syntax"
-wait_random = getattr(importlib.import_module(module_name), 'wait_random')
+wait_r = __import__('0-basic_async_syntax').wait_random
 
 
 def task_wait_random(max_delay: int) -> asyncio.Task:
     """
-    Creates an asyncio.Task from the wait_random coroutine.
+    Creates an asyncio Task that runs the wait_random coroutine.
 
     Args:
-        max_delay (int): Maximum delay for the wait_random coroutine.
+        max_delay (int): The maximum delay (in seconds) for the
+        wait_random coroutine.
 
     Returns:
-        asyncio.Task: An asyncio Task object.
+        asyncio.Task: An asyncio Task object that runs the
+        wait_random coroutine.
+
+    Description:
+        This function uses the `asyncio.create_task()` function to
+        create a task from the `wait_random` coroutine, which is
+        imported from the '0-basic_async_syntax' module using the
+        `__import__()` function. The task is scheduled to run
+        concurrently, and the function immediately returns the Task object.
+
+    Example:
+        task = task_wait_random(5)
+        # The above line creates an asyncio Task that will wait
+        # for a random delay between 0 and 5 seconds.
     """
-    return asyncio.create_task(wait_random(max_delay))
+    return asyncio.create_task(wait_r(max_delay))
