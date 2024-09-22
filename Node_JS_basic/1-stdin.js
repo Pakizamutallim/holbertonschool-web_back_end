6348c7be6d2c78d8bd3e4868cb696a5c00e21bd9
PakizaMutallimova <pakizemutellimova97@mail.ru>
2024-09-22 16:02:34 +0400

    added

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,19 +1,15 @@
-// Display initial message
-console.log("Welcome to Holberton School, what is your name?");
-
-// Listen for user input from stdin
-process.stdin.on('data', (input) => {
-  // Convert the input buffer to a string and remove trailing spaces or newline characters
-  const name = input.toString().trim();
-
-  // Display the user's name
-  console.log(`Your name is: ${name}`);
-
-  // Close the input stream and display the closing message
-  process.exit();
-});
-
-// Handle process exit and display a message when the program closes
-process.on('exit', () => {
-  console.log("This important software is now closing");
+console.log('Welcome to Holberton School, what is your name?');
+// await input
+process.stdin.resume();
+// once input is entered
+process.stdin.on('readable', () => {
+  const name = process.stdin.read();
+  process.stdout.write(`Your name is: ${name}`);
+  // check if input is coming from the terminal
+  if (process.stdin.isTTY) {
+    process.exit();
+  } else {
+    process.stdout.write('This important software is now closing\n');
+    process.exit();
+  }
 });
